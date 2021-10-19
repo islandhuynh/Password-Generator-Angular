dd7233df702eea24a7e8ce193367022204852c08
Island <islandhuynh@gmail.com>
2021-10-19 00:45:45 -0400

    Added event handlers to keep track of password length and if letters, numbers, and/ or symbols are used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    console.log(this.includeLetters)
+    console.log(this.includeNumbers)
+    console.log(this.includeSymbols)
+    console.log(this.length)
+    this.password = 'MY PASSWORD!!!';
+  }
 }
